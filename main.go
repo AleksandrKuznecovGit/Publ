78f87ddc4d3254d80e33d87b067158f0baa5ae4d
”AleksandrKuznecovGit” <kuz888ka147@gmail.com>
2022-05-08 13:28:16 +0700

    add

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 	"net/http"
 )
 
-func echoFunction(w http.ResponseWriter, r *http.Request) {
+func echoString(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello,Geekbrains")
 }
 
 func main() {
-	http.HandleFunc("/", echoFunction)
+	http.HandleFunc("/", echoString)
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "OK")
