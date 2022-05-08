f85e16eda5493db1bad109a0e317e84de1f0e1d3
”AleksandrKuznecovGit” <kuz888ka147@gmail.com>
2022-05-08 12:04:29 +0700

    add health check

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 		fmt.Fprint(w, "Hello,Geekbrains")
 	})
 
+	http.HandleFunc("/healt", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "OK")
+	})
+
 	log.Fatal(http.ListenAndServe(":8085", nil))
 
 }
